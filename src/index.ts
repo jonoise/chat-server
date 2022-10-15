99c5b124149cf989ec826582631583fa5215d23e
Amilcar Munoz <yellowballpython@gmail.com>
2022-10-15 03:07:52 -0600

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { Server } from 'socket.io'
 import express from 'express'
 import http from 'http'
 import os from 'os'
+import cors from 'cors'
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server, { cors: { origin: '*' } })
 const PORT = process.env.PORT || 4000
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>')
 })
